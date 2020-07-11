0c0c1646fca28e71b82ff5cd9169ca4a2b7fd218
Sed458 <sed458@nyu.edu>
2020-07-11 17:48:01 -0400

    updated students-list

--- src/app/components/students-list/students-list.component.ts
+++ src/app/components/students-list/students-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Student } from './../../shared/student';
+import { ApiService } from './../../shared/api.service';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-students-list',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students-list.component.css']
 })
 export class StudentsListComponent implements OnInit {
+  StudentData: any = [];
+  dataSource = new MatTableDataSource<Student>();
+  @ViewChild(MatPaginator) pagniator: MatPaginator;
+  displayedColumns: string[] = ['_id', 'student_name', 'student_email', 'section', 'action'];
 
-  constructor() { }
+  constructor(private studentApi: ApiService) { 
+    this.studentApi.GetStudents().subscribe(data => {
+      this.StudentData = data;
+      this.dataSource = new MatTableDataSource<Student>(this.StudentData);
+      setTimeout(() => {
+        this.dataSource.paginator = this.pagniator;
+      }, 0);
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  deleteStudent(index: number, e) {
+    if(window.confirm('Are you sure')) {
+      const data = this.dataSource.data;
+      data.splice((this.pagniator.pageIndex * this.pagniator.pageSize) + index, 1);
+      this.dataSource.data = data;
+      this.studentApi.DeleteStudent(e.id).subscribe()
+    }
+  }
 }
